0cc79cb9f2f2f8b7a5f94ec64d7fa20ff02206e9
ZmjDns <292570927@qq.com>
2020-04-01 17:27:23 +0800

    Base.

--- baselibray/src/main/java/com/zmj/baselibray/BaseApplication.java
+++ baselibray/src/main/java/com/zmj/baselibray/BaseApplication.java
@@ -11,16 +11,8 @@ import android.app.Application;
  */
 public class BaseApplication extends Application {
 
-    private static BaseApplication baseApplication;
-
     @Override
     public void onCreate() {
         super.onCreate();
-
-        baseApplication = this;
-    }
-
-    public static BaseApplication getBaseApplication(){
-        return baseApplication;
     }
 }
---
+++ baselibray/src/main/java/com/zmj/baselibray/config/Const.kt
@@ -0,0 +1,17 @@
+package com.zmj.baselibray.config
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/3/27
+ * Description :
+ */
+object Const {
+    const val BASE_URL = "https://www.wanandroid.com"
+
+    const val DEFAULT_TIME_OUT = 10L
+
+
+
+}
