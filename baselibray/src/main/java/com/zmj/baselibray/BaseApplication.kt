1b8fbd573cab5e6cc952b2f12bfb0101a4b56408
ZmjDns <292570927@qq.com>
2020-04-06 23:11:40 +0800

    BaseApplication fix

---
+++ baselibray/src/main/java/com/zmj/baselibray/BaseApplication.kt
@@ -0,0 +1,22 @@
+package com.zmj.baselibray
+
+import android.app.Application
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2020/4/6
+ * Description :
+ */
+open class BaseApplication: Application() {
+
+    companion object{
+        lateinit var baseApplication: Application
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        baseApplication = this
+    }
+}
